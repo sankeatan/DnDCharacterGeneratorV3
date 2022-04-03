18b55d96d9703e8163f9493f134e2116a1a06d7c
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-04-03 19:20:42 -0400

    setup react, added js for characater cards in the 5e style

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CharacterCard from './characterCard'
+import Header from './header'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <CharacterCard/>
+    </div>
+  );
+}
+
+export default App;
