a4328ad49b7163d2258ab422ace1d3b35ccb7767
sankeatan <47090053+sankeatan@users.noreply.github.com>
2022-04-12 19:14:55 -0400

    some small changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import CharacterEditor from './CharacterEditor'
-//import Header from './header'
+import { useState } from 'react'
+import { characterData } from './characterData';
+import CharacterEditor from './characterEditor'
 import './App.css';
 
 function App() {
+  const [character, setCharacter] = useState({ characterData })
+
+  const updateCharacter = ( value ) => {
+    let updatedValue = {};
+    updatedValue = {value}
+    setCharacter(character => ({
+      ...character,
+      updatedValue
+    }));
+  }
+
   return (
-    <CharacterEditor />
+    <div>
+    <CharacterEditor character={character} updateCharacter={updateCharacter}/>
+    </div>
   );
 }
 
